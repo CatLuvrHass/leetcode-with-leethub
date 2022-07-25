e9cf2a43f29a23e8d409281073b1ba7ecc816001
Hassan Albujasim <72608789+CatLuvrHass@users.noreply.github.com>
2022-07-25 11:45:14 +0100

    Time: 143 ms (37.78%), Space: 15.5 MB (50.23%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,14 +1,25 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        
-        l, r = 0, len(nums) - 1
-        
-        while(l <= r):
-            if nums[l] == target and nums[r] == target:
-                return [l, r]
-            elif nums[l] < target:
-                l += 1
-            elif nums[r] > target:
-                r -= 1
-        
-        return [-1,-1]
+
+        """
+        method: bianry search n(log n)
+
+        lo search(target) will return lower index if it finds
+
+        search(target+1), which tells us the first index 
+        where we could insert target+1, which of course 
+        is one index behind the last index containing target, 
+        so all I have left to do is subtract 1
+
+        """
+        def search(n):
+            lo, hi = 0, len(nums)
+            while lo < hi:
+                mid = (lo + hi) // 2
+                if nums[mid] >= n:
+                    hi = mid
+                else:
+                    lo = mid + 1
+            return lo
+        lo = search(target)
+        return [lo, search(target+1)-1] if target in nums[lo:lo+1] else [-1, -1]
