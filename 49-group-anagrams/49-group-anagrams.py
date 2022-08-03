51fcb79878be393b8ff2fbf25b41594ae3a98b43
Hassan Albujasim <72608789+CatLuvrHass@users.noreply.github.com>
2022-08-03 16:11:46 +0100

    Time: 156 ms (56.61%), Space: 17.7 MB (67.02%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -1,13 +1,14 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        """
+        each word that is anagram append to sorted key_word
         
-        def isAnagram(s):
-            return sorted(s)
-        
+        return the values()
+        """
         anagrams = {}
         
         for word in strs:
-            sorted_word = ''.join(isAnagram(word))
+            sorted_word = ''.join(sorted(word))
             if sorted_word not in anagrams:
                 anagrams[sorted_word] = [word]
             else:
